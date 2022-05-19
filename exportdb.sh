b935fb2576afe8bd1cf11eec6782f219d4b6502e
kevinebio <kvndvdsnebio@hiro-c.net>
2022-05-19 15:58:59 +0800

    added hiro-api server as option to export

--- exportdb.sh
+++ exportdb.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 
 hiroTest='test-aurora-mysql-57.cluster-caqvsmapmeil.ap-northeast-1.rds.amazonaws.com'
+hiroAp= 'mysql -hapi-aurora-mysql-57.cluster-caqvsmapmeil.ap-northeast-1.rds.amazonaws.com'
 
 echo "Servers: "
 sed -rn 's/^\s*Host\s+(.*)\s*/\1/ip' ~/.ssh/config
@@ -29,7 +30,7 @@ read server
 				read backup
 
 				echo "user input: ${backup}"
-				
+
 				##### DROPPING EXISTING DATABASE #####
 
 
@@ -49,6 +50,47 @@ read server
 			else
 				echo "You've entered an invalid option. Please re-run the script. Thank you!";
 			fi
-	else
-		echo "${server} is not yet condigured. Thank you."
+	elif [ $server = "hiro-api" ];
+		then
+		echo "***** Great! Successfully connected to ${server} *****"
+		echo ">> Which database would you like to connect? <<"
+		read dbname
+
+		echo ">> Would you like to import or export ${dbname}? <<"
+		read action
+			if [ $action = "import" ];
+			then
+				##### Back up current database #####
+				ssh ${server} mysqldump --host $hiroApi -u admin -p ${dbname} > "${dbname} -$(date +"%Y-%m-%d-%H%M%S").sql"
+				echo "Backup successful and saved in: ${PWD}"
+
+				##### UPLOADING BACKUP FILE TO REMOTE SERVER #####
+
+				echo ">> Here are the list of backup on your local machine: "
+				echo "$( ls | grep *.sql )"
+
+				echo "Which backup would you like to import?"
+				read backup
+
+				echo "user input: ${backup}"
+
+				##### DROPPING EXISTING DATABASE #####
+
+
+
+				##### RECREATE THE DATABASE #####
+
+			elif [ $action = "export" ];
+			then
+
+				##### WITH COMPRESSION #####
+				ssh ${server} mysqldump --host $hiroApi -u admin -p ${dbname} | gzip --best > "${dbname} -$(date +"%Y-%m-%d-%H%M%S").sql.gz"
+
+				##### WITHOUT COMPRESSION #####
+				# ssh ${server} mysqldump --host $hiroApi -u admin-p ${dbname} > "${dbname} -$(date +"%Y-%m-%d-%H%M%S").sql"
+				echo "Backup successful and saved in: ${PWD}"
+				
+			else
+				echo "You've entered an invalid option. Please re-run the script. Thank you!";
+			fi
 	fi
